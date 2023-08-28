bb62992bee00c8d3ca0ebb48a0e29a5c29e52ec5
Richard Mills <me@richardmills.ca>
2023-08-28 15:39:46 -0700

    style: use noise reduced images

--- src/lib/Environment.svelte
+++ src/lib/Environment.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { Environment } from '@threlte/extras'
 
-  const files = [...Array(6).keys()].map((i) => `/env-${i}.png`)
+  const files = [...Array(6).keys()].map((i) => `/env-${i}-nr.png`)
 </script>
 
 <Environment {files} isBackground />
