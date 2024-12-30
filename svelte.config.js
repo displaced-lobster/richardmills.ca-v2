54baef8847c0ba87a42bf7c1e7d1d0e0360ad5f0
Richard Mills <me@richardmills.ca>
2024-12-30 15:38:51 -0800

    chore: upgrade svelte, sveltekit, and threlte

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import seqPreprocessor from 'svelte-sequential-preprocessor'
-import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
