b65d6f7c7916ac72bb0f1aec8d8cd9dbe15ca559
Richard Mills <me@richardmills.ca>
2024-12-30 14:39:46 -0800

    refactor: name files by position

--- src/lib/Environment.svelte
+++ src/lib/Environment.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { Environment } from '@threlte/extras'
 
-  const files = [...Array(6).keys()].map((i) => `/env-${i}-nr.png`)
+  const files = ['front', 'back', 'top', 'bottom', 'right', 'left'].map((dir) => `${dir}.png`)
 </script>
 
 <Environment {files} isBackground />
