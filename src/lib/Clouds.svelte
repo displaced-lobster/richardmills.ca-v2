e9f7c2fd006e9a77c8243938032ceff82dc342c7
Richard Mills <me@richardmills.ca>
2024-12-30 14:40:27 -0800

    feat: add clouds

---
+++ src/lib/Clouds.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { T } from '@threlte/core'
+  import fragmentShader from '$lib/shaders/fragment.glsl?raw'
+  import vertexShader from '$lib/shaders/vertex.glsl?raw'
+</script>
+
+<T.Mesh {...$$restProps}>
+  <T.IcosahedronGeometry args={[1, 5]} />
+  <T.ShaderMaterial transparent {fragmentShader} {vertexShader} />
+</T.Mesh>
--- src/lib/PlanetGroup.svelte
+++ src/lib/PlanetGroup.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { T, useFrame } from '@threlte/core'
+  import Clouds from '$lib/Clouds.svelte'
   import Planet from '$lib/Planet.svelte'
 
   let rotation = 0
@@ -10,5 +11,6 @@
 </script>
 
 <T.Group {...$$restProps} rotation.y={rotation}>
+  <Clouds scale={1.01} />
   <Planet />
 </T.Group>
